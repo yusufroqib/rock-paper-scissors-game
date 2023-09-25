e2fc0f55d02de9721f288581c59ebc77f221a2f6
yusufroqib <yusufroqib@gmail.com>
2023-09-25 16:00:28 +0100

    Update

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ const showWinner = (winner, computerChoice) => {
     result.innerHTML = ` 
     <h1 class="text-win">You Win</h1>
     <i id="${computerChoice}" class="choice fas fa-hand-${computerChoice} fa-10x"></i>
-    <p>Computer chose <strong>${computerChoice}</strong></p>
+    <p>Computer Chose <strong>${computerChoice.charAt(0).toUpperCase()+computerChoice.slice(1)}</strong></p>
     `;
   } else if (winner === "computer") {
     //increase by score
@@ -70,13 +70,13 @@ const showWinner = (winner, computerChoice) => {
     result.innerHTML = ` 
     <h1 class="text-win">You Lose</h1>
     <i id="${computerChoice}" class="choice fas fa-hand-${computerChoice} fa-10x"></i>
-    <p>Computer chose <strong>${computerChoice}</strong></p>
+    <p>Computer Chose <strong>${computerChoice.charAt(0).toUpperCase()+computerChoice.slice(1)}</strong></p>
     `;
   } else {
     result.innerHTML = `
         <h1>It is a draw</h1>
         <i id="${computerChoice}" class="choice fas fa-hand-${computerChoice} fa-10x"></i>
-        <p>Computer chose <strong>${computerChoice}</strong></p>
+        <p>Computer Chose <strong>${computerChoice.charAt(0).toUpperCase()+computerChoice.slice(1)}</strong></p>
         `;
   }
 
